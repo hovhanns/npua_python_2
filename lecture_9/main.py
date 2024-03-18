f508d8aafe61a6290bb29604ae425037ad052a3f
Harutyun Hovhannisyan <haruthovhanns@gmail.com>
2024-03-18 11:08:47 +0400

    lecture_9

---
+++ lecture_9/main.py
@@ -0,0 +1,7 @@
+from module1 import my_function, my_function2
+import random
+import requests
+# print("This is my function from main.py")
+
+my_function()
+
---
+++ lecture_9/module1.py
@@ -0,0 +1,10 @@
+def my_function():
+    print("This is my function")
+
+def my_function2():
+    print("This is my function")
+
+
+if __name__ == "__main__":
+    print("This is my function from module1.py")    
+    my_function()
